5a7d1026675d050738d53162e6fd69f535d37f06
tetsu1520 <59831539+tetsu1520@users.noreply.github.com>
2020-01-16 15:30:15 +0900

    Update Untitled-1.js

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,14 +1,24 @@
-function(){
-    
-    $('menu a').click(function(){
-        var = id =$(this).attr('href');
-        var = position =$(id).offset().top;
-        $('html body').animate{
-            'scrollTop':position
-        },2000);
-    });
-}
-
 $(function(){
-    $('.top-wrapper').hide().fadeIn('slow');
-});
+    $('a[href^="#"]').click(function(){
+      var speed = 500;
+      var href= $(this).attr("href");
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      var position = target.offset().top;
+      $("html, body").animate({scrollTop:position}, speed, "swing");
+      return false;
+    });
+
+
+    setTimeout(function(){
+		$('.start p').fadeIn(1600);
+	},500); //0.5秒後にロゴをフェードイン!
+	setTimeout(function(){
+		$('.start').fadeOut(500);
+    },3500);
+    $(function(){
+        $('top-container').fadeIn('slow');
+    });
+
+
+
+  });
