fbe5f2171d06369a08ad9fb8f25609349983146d
tetsu1520 <59831539+tetsu1520@users.noreply.github.com>
2020-01-17 09:37:39 +0900

    Update Untitled-1.js

--- Untitled-1.js
+++ Untitled-1.js
@@ -9,7 +9,58 @@ $(function(){
     });
 
 
-    
+    setTimeout(function(){
+		$('.start p').fadeIn(1600);
+	},500); //0.5秒後にロゴをフェードイン!
+	setTimeout(function(){
+		$('.start').fadeOut(500);
+    },3500);
+    $(function(){
+        $('top-container').fadeIn('slow');
+    });
+
+    var topBtn = $('#page-top');    
+    topBtn.hide();
+    //スクロールが100に達したらボタン表示
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            topBtn.fadeIn();
+        } else {
+            topBtn.fadeOut();
+        }
+    });
+    //スクロールしてトップ
+    topBtn.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+
+    var setElm = $('.top-container'),
+    delaySpeed = 200,
+    fadeSpeed = 0;
+ 
+    setText = setElm.html();
+ 
+    setElm.css({visibility:'visible'}).children().addBack().contents().each(function(){
+        var elmThis = $(this);
+        if (this.nodeType == 3) {
+            var $this = $(this);
+            $this.replaceWith($this.text().replace(/(\S)/g, '<span class="textSplitLoad">$&</span>'));
+        }
+    });
+    $(window).load(function(){
+        splitLength = $('.textSplitLoad').length;
+        setElm.find('.textSplitLoad').each(function(i){
+            splitThis = $(this);
+            splitTxt = splitThis.text();
+            splitThis.delay(i*(delaySpeed)).css({display:'inline-block',opacity:'0'}).animate({opacity:'1'},fadeSpeed);
+        });
+        setTimeout(function(){
+                setElm.html(setText);
+        },splitLength*delaySpeed+fadeSpeed);
+    });
 
 
   });
